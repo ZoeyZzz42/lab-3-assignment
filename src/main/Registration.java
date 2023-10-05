9006091f65e0e6d9f705bcc050bad050bbbfb0e2
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2023-10-04 20:08:33 -0400

    refactor project

--- src/main/Registration.java
+++ src/main/Registration.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package swing;
+package main;
 
 /**
  *
  * @author zoey42
  */
-public class Lab3Assignment {
+public class Registration {
 
     /**
      * @param args the command line arguments
